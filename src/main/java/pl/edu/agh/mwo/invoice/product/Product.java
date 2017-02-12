837febc158304c1df9c3c8ec2dac78901711ac62
Student1 <Student1@S7N-PROGLANG-19.pcoip.ki.agh.edu.pl>
2017-02-12 14:59:23 +0100

    Fifth test fixed

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -11,10 +11,16 @@ public abstract class Product {
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
 		this.name = name;
+		if (name==null || name==""){
+			throw new IllegalArgumentException("Empty product name!");
+		}
 		this.price = price;
+		if (price==null || price.signum()<0){
+			throw new IllegalArgumentException("Incorrect product price!");
+		}
 		this.taxPercent = tax;
 	}
-
+		
 	public String getName() {
 		return this.name;
 	}
